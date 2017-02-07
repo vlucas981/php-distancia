5fd300fcbf0f9a901854ea290626d57e22b2242a
Victor Lucas <vlucas981@gmail.com>
2017-02-07 01:42:38 -0200

    criação do arquivo tipos-de-dados.php, para conhecer todos os tipos de dados do php

---
+++ tipos-de-dados.php
@@ -0,0 +1,27 @@
+<?php
+
+// numeros inteiros - int
+$variavel = 13;
+
+// numeros com ponto flutuante - float
+$variavel = 13.10;
+
+// strings
+$variavel = "Victor Lucas";
+$variavel = 'Victor Lucas';
+
+// boleanos - bool
+$variavel = true;
+$variavel = false;
+
+// arrays
+$variavel = array(1, 2, 3, 10.1, 'Victor', true);
+
+// objetos
+//$variavel = new Objeto();
+
+// recursos
+$variavel = mysql_connect('localhost', 'root');
+
+// nulo
+$variavel = null;
